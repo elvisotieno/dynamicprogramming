16e907056423c85ea5b94ecba2682e43d9985321
elvisotieno <janetchepngetich96@gmail.com>
2021-12-07 06:36:09 +0300

    updated backtracking

--- dynamic programming/backtracking.py
+++ dynamic programming/backtracking.py
@@ -23,11 +23,12 @@ first_combination=PossibleCombinations()
 print(first_combination.combine(4,2))
 
 
+# All possible arrangements satisfying a given condition
 class ChaseGame:
     def n_queens(self,n):
         #all the information we need
         col =set()
-        postive_diago=set()     #determined by (row+ol)
+        postive_diago=set()     #determined by (row+col)
         negative_diago=set()    #determined by (row-col)
 
         result=[]
@@ -41,6 +42,7 @@ class ChaseGame:
 
                 return
             #else we didn't reacg the base-case hence we need to continue
+            #loop through the columns for the next row
             for c in range(n):
                 if c in col or (r+c) in postive_diago or (r-c) in negative_diago:
                     continue
@@ -62,3 +64,58 @@ class ChaseGame:
 
 queens=ChaseGame()
 print(queens.n_queens(4))
+
+class SubsetProblems:
+    def power_subset(self,nums):
+        result = []
+
+        current_subset=[]
+        def backtrack(i):
+            if i >= len(nums): # means we got a valid subset(base-case)
+                result.append(current_subset.copy())
+                return
+
+            #decision to include nums[i]
+            current_subset.append(nums[i])
+            backtrack(i+1)
+
+            #decision not to include nums[i]
+            current_subset.pop()
+            backtrack(i+1)
+
+        backtrack(0)
+        return result
+
+    def subset_sum_to_target(self,arr,target):
+        result = []
+
+
+        def backtrack(weights):
+            if sum(weights)==target: # we got a valid subset
+                result.append(list(weights))
+            other_weights = set(arr).difference(weights)
+            #chosing weights in an increasing order to avoid duplication of results
+            if len(weights)>0:
+                other_weights = [W for W in other_weights if W > max(weights)]
+            for weight in other_weights:
+                # bounding function
+                if sum(weights) + weight <= target:
+                    #decision to include weight
+                    weights.add(weight)
+                    backtrack(weights)
+                    #decision not to include weight
+                    weights.remove(weight)
+
+
+        backtrack(set())
+        return result
+
+
+all_possible_subsets = SubsetProblems()
+nums = [1,2,3]
+print(all_possible_subsets.power_subset(nums))
+arr=[5,10,12,13,15,18]
+target=30
+print(all_possible_subsets.subset_sum_to_target(arr,target))
+
+

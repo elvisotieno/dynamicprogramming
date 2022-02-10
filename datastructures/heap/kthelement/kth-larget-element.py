a16d42e88a36532ef460aa4c8eddb3a505432054
elvisotieno <janetchepngetich96@gmail.com>
2022-02-10 17:20:12 +0300

    added kth largest and kth smallest using heap

--- datastructures/heap/kthelement/kth-larget-element.py
+++ datastructures/heap/kthelement/kth-larget-element.py
@@ -9,6 +9,18 @@ def kth_largest(nums,k):
         heapq.heappush(given,largest)
     return heapq.heappop(given)
 
+
+def kth_smallest(nums,k):
+    nums =[nums[i]*-1 for i in range(len(nums))]
+    given = nums[:k]
+    heapq.heapify(given)
+    for i in range(k,len(nums)):
+        node = heapq.heappop(given)
+        smallest = max(node,nums[i])
+        heapq.heappush(given,smallest)
+    return heapq.heappop(given)*-1
+
 nums=[3,2,1,6,4,5]
 k=2
 print(kth_largest(nums,k))
+print(kth_smallest(nums,k))
